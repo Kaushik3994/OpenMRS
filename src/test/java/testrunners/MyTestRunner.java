610ffad234c6197d5407734fa9c0617e569769c0
supriyaponugupati-sherwin <supriya.ponugupati@sherwin.com>
2023-08-21 15:07:57 +0530

    updated the reports

--- src/test/java/testrunners/MyTestRunner.java
+++ src/test/java/testrunners/MyTestRunner.java
@@ -1,11 +1,15 @@
 package testrunners;
 
+import com.qa.util.DriverFactory;
+import helpers.ExecutionHelper;
 import io.cucumber.testng.CucumberOptions;
 import org.junit.runner.RunWith;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.DataProvider;
+
 
 
 @CucumberOptions(
@@ -17,6 +21,17 @@ import org.testng.annotations.DataProvider;
 
 )
 public class MyTestRunner extends AbstractTestNGCucumberTests{
+
+	@AfterMethod(alwaysRun=true)
+	public void endTestExecution(){
+		DriverFactory.getDriver().quit();
+		try {
+			ExecutionHelper.extentReportFlush();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	@Override
 
 	@DataProvider(parallel = false)
